7e9d7b3bc911592bea56be7f32e48be6ba2d237b
AmaliJD <amali.jdelauney@gmail.com>
2026-03-16 21:46:08 -0400

    mousedown instead of click

--- static/js/strip-icons.js
+++ static/js/strip-icons.js
@@ -1,5 +1,5 @@
 document.querySelectorAll('.strip-icon').forEach(img => {
-        img.addEventListener('click', () => {
+        img.addEventListener('mousedown', () => {
             img.classList.remove('is-fading');
             // Prevent restarting mid-spin if you want, or just re-add
             var rand = Math.random() * 4;
